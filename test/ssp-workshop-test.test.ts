fbfaf51ad35c25fd10274d9f83911c7da2949fcc
Apoorva Kulkarni <kuapoorv@amazon.com>
2022-01-18 20:08:47 +0000

    adding stacks

---
+++ test/ssp-workshop-test.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as SspWorkshopTest from '../lib/ssp-workshop-test-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ssp-workshop-test-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SspWorkshopTest.SspWorkshopTestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
